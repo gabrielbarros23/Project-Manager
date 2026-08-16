33efaeb404f1e47636a53f0b4b2ab46b57925dd5
Gabriel Barros Bezerra <gabrielbarross2311@gmail.com>
2026-08-16 20:14:15 -0300

    feat: import user, project and taks modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './routes/user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma.service';
+import { AuthModule } from './routes/auth/auth.module';
+import { ProjectModule } from './routes/project/project.module';
 
 @Module({
-  imports: [UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [UserModule,PrismaModule, AuthModule,ConfigModule.forRoot({ isGlobal: true, }), ProjectModule],
+    controllers: [AppController],
+    providers: [AppService ],
 })
 export class AppModule {}
